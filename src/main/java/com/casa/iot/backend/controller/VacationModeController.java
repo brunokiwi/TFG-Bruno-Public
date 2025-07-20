5884a6c0399e77770b139b1c8181b35b4e3f48c0
Bruno <brunobenya12@gmail.com>
2025-07-20 21:49:36 +0200

    commit inicial, nuevo endpoint

---
+++ src/main/java/com/casa/iot/backend/controller/VacationModeController.java
@@ -0,0 +1,5 @@
+package com.casa.iot.backend.controller;
+
+public class VacationModeController {
+    
+}
---
+++ src/main/java/com/casa/iot/backend/service/VacationModeService.java
@@ -0,0 +1,5 @@
+package com.casa.iot.backend.service;
+
+public class VacationModeService {
+    
+}

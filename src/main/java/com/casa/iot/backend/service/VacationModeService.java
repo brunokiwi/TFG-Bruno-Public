a79dce9e8d2453965daef4ff97c48ec3e0613bac
Bruno <brunobenya12@gmail.com>
2025-07-26 01:06:38 +0200

    vacation mode logging

--- src/main/java/com/casa/iot/backend/service/VacationModeService.java
+++ src/main/java/com/casa/iot/backend/service/VacationModeService.java
@@ -18,14 +18,16 @@ public class VacationModeService {
     private final RoomRepository roomRepository;
     private final MovementService movementService;
     private final LightService lightService;
+    private final EventLogService eventLogService;
     private final AtomicBoolean vacationModeActive = new AtomicBoolean(false);
     private final Random random = new Random();
     private List<String> roomNamesCache = new ArrayList<>();
     
-    public VacationModeService(RoomRepository roomRepository, MovementService movementService, LightService lightService) {
+    public VacationModeService(RoomRepository roomRepository, MovementService movementService, LightService lightService, EventLogService eventLogService) {
         this.roomRepository = roomRepository;
         this.movementService = movementService;
         this.lightService = lightService;
+        this.eventLogService = eventLogService;
     }
     
     public void activateVacationMode() {
@@ -35,6 +37,10 @@ public class VacationModeService {
                                        .map(Room::getName)
                                        .collect(Collectors.toList());
         activateAllMotionSensors();
+        // logging
+        String details = String.format("{\"method\":\"SYSTEM\",\"timestamp\":\"%s\",\"sensors\":\"ALL_ACTIVATED\"}", 
+                                       java.time.LocalDateTime.now());
+        eventLogService.logSystemAction("VACATION_MODE_ACTIVATED", null, details, "VACATION_MODE");
     }
     
     public void deactivateVacationMode() {
@@ -46,6 +52,10 @@ public class VacationModeService {
         for (Room room : rooms) {
             movementService.sendAlarmCommand(room.getName(), false);
         }
+        // logging
+        String details = String.format("{\"method\":\"SYSTEM\",\"timestamp\":\"%s\"}", 
+                                       java.time.LocalDateTime.now());
+        eventLogService.logSystemAction("VACATION_MODE_DEACTIVATED", null, details, "VACATION_MODE");
     }
     
     public boolean isVacationModeActive() {
